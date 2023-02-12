077177172f0a5d79ecccbb2df146fd6cf87c8efe
artdian <46709199+jartdian@users.noreply.github.com>
2023-02-12 18:41:15 +0100

    misc styling

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const inputValue = document.querySelector(".cellAmountInput");
 const colorPickerInput = document.querySelector('.colorPickerInput')
 
 function draw() {
-  // this.classList.add('drawn-cell');
   let clr = `${colorPickerInput.value}` 
-  console.log(clr);
   this.style.cssText += `background-color: ${clr}`
+}
+function onMouseOver() {
+  this.classList.add("cell-hovered")
+}
+function onMouseOut() {
+  this.classList.remove("cell-hovered")
 }
 
 function occupyGrid() {
@@ -17,6 +21,8 @@ function occupyGrid() {
   for (let i = 0; i < inputValue.valueAsNumber ** 2; i++) {
 		let cell = document.createElement("div");
 		cell.addEventListener("click", draw);
+    cell.addEventListener("mouseover", onMouseOver)
+    cell.addEventListener("mouseout", onMouseOut)
     cell.classList.add("cell");
     cell.style.cssText = `width: ${container.offsetWidth / inputValue.value}px;
 			height: ${container.offsetWidth / inputValue.value}px;`;
